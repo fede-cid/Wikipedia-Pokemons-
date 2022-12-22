7f37a9a612d281aec293cbf109ad75dd24f2e367
fede-cid <fede.cid.96@gmail.com>
2022-12-22 01:33:46 -0300

    front3

--- client/src/Redux/Reducer/Index.js
+++ client/src/Redux/Reducer/Index.js
@@ -16,35 +16,35 @@ const initialState = {
 
 const rootReducer = (state = initialState, action) => {
     switch (action.type) {
-      case GET_POKEMON:
-        return {
-          ...state,
-          characterDetail: action.payload,
-        };
+        case GET_POKEMON:
+          return {
+            ...state,
+            pokemons: action.payload,
+          };
       case GET_POKEMON_DETAIL_ID:
         return {
           ...state,
-          characters: action.payload,
+          pokemonsDetails: action.payload,
         };
       case GET_POKEMON_DETAIL_NAME:
         return {
           ...state,
-          characters: action.payload,
+          pokemonsDetails: action.payload,
         };
       case CREATE_POKEMON:
         return {
           ...state,
-          characters: [...state.characters, action.payload],
+          pokemons: [...state.pokemons, action.payload],
         };
       case DELETE_POKEMON:
         return {
           ...state,
-          characters: state.characters.filter((c) => c.id !== action.payload),
+          pokemons: state.pokemons.filter((c) => c.id !== action.payload),
         };
       case GET_TYPE:
         return {
           ...state,
-          ships: action.payload,
+          types: action.payload,
         };
   
       default:
