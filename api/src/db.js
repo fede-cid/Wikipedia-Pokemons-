68ed96dd718ece33fbc18d39c42dbc6d1bce4733
fede-cid <fede.cid.96@gmail.com>
2023-01-09 22:29:44 -0300

    edit

--- api/src/db.js
+++ api/src/db.js
@@ -2,10 +2,10 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST , DB_NAME} = process.env;
+// const { DB_USER, DB_PASSWORD, DB_HOST , DB_NAME} = process.env;
 
 const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+  `postgres://pokemon_t3w5_user:I7bg8SlYzPvABjB42QCfRu0YsLEGnpHb@dpg-ceubbksgqg40d6headvg-a.oregon-postgres.render.com/pokemon_t3w5`,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
