53327be00127140f4751066bc91f2b9a16bf99ae
fede-cid <fede.cid.96@gmail.com>
2023-01-09 22:37:36 -0300

    edit 15

--- api/src/db.js
+++ api/src/db.js
@@ -5,7 +5,7 @@ const path = require("path");
 // const { DB_USER, DB_PASSWORD, DB_HOST , DB_NAME} = process.env;
 
 const sequelize = new Sequelize(
-  `postgres://pokemon_t3w5_user:I7bg8SlYzPvABjB42QCfRu0YsLEGnpHb@dpg-ceubbksgqg40d6headvg-a.oregon-postgres.render.com/pokemon_t3w5`,
+  `postgres://pokemon_t3w5_user:I7bg8SlYzPvABjB42QCfRu0YsLEGnpHb@dpg-ceubbksgqg40d6headvg-a/pokemon_t3w5`,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
